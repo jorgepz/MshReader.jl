029f353900fd9152b55311265435a1b41ae25bd0
jorgepz <jorgepz@fing.edu.uy>
2022-07-03 09:26:42 -0300

    add test and fix bug

--- docs/src/index.md
+++ docs/src/index.md
@@ -4,7 +4,18 @@ CurrentModule = MshReader
 
 # MshReader
 
-Documentation for [MshReader](https://github.com/jorgepz/MshReader.jl).
+## What is MshReader?
+
+[MshReader](https://github.com/jorgepz/MshReader.jl) is a minimal package for reading [GMSH](https://gmsh.info/) mesh files. The input files are assumed to be given by the msh version 4.1 format, explained in [this web](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format). 
+
+The goal of this package is to obtain nodal coordinates and element connectivities, as well as physical properties assigned to each element.
+
+## Who is writing MshReader?
+
+By the moment, only [Jorge Pérez Zerpa](https://www.fing.edu.uy/~jorgepz/). Any contribution is welcome.
+
+## Functions list
+
 
 ```@index
 ```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,24 @@
 using MshReader
 using Test
+using Downloads:download
 
-@testset "MshReader.jl" begin
-    
-    nodesCoordMat, connectivity, physicalNames, elemPhysNums = MshFileReader( joinpath("..", "examples", "pyramid.msh") )
+@testset "MshReader" begin
 
-    @test nodesCoordMat[3,:] == [1.0,1.0,0]
-    @test nodesCoordMat[5,:] == [0.5,0.5,0.5]
+    @testset "Pyramid problem tests" begin
 
-    @test cmp( physicalNames[ elemPhysNums[1]], "point_prop_1" ) == 0
+        nodesCoordMat, connectivity, physicalNames, elemPhysNums = MshFileReader( joinpath("..", "examples", "pyramid.msh") )
 
-    tmp_location = download("https://github.com/ONSAS/ONSAS.m/raw/master/examples/uniaxialExtension/geometry_uniaxialExtension.msh")
+        @test nodesCoordMat[3,:] == [1.0,1.0,0]
+        @test nodesCoordMat[5,:] == [0.5,0.5,0.5]
+        @test cmp( physicalNames[ elemPhysNums[1]], "point_prop_1" ) == 0
+    end
 
-    nodesCoordMat, connectivity, physicalNames, elemPhysNums = MshFileReader( tmp_location )
+    @testset "Uniaxialextension problem tests" begin
 
+        tmp_location = download("https://github.com/ONSAS/ONSAS.m/raw/master/examples/uniaxialExtension/geometry_uniaxialExtension.msh")
+
+        nodesCoordMat, connectivity, physicalNames, elemPhysNums = MshFileReader( tmp_location; verbosityBoolean=false )
+
+        @test nodesCoordMat[7,:] == [2.0,1.0,1.0]
+    end
 end
